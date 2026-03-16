b08a063ce61475f8127bfd067bd967f95c8873c6
sam mak <sm972312@gmail.com>
2026-03-16 12:31:41 +0530

    sdf

--- src/api/blogApi.ts
+++ src/api/blogApi.ts
@@ -47,7 +47,7 @@ export const blogData: BlogItem[] = [
         }`,
 
     category: "Marketing",
-    heading: "Boost's your \"conversion rate\"",
+    heading: "1221Boost's your \"conversion rate\"",
     content: "Illo sint voluptas. Error voluptates culpa eligendi. ",
     Megacontent: "Illo sint voluptas. Error voluptates culpa eligendi. Hic vel totam vitae illo. Non aliquid explicabo necessitatibus unde. Sed exercitationem placeat consectetur nulla deserunt vel. Iusto corrupti dicta.",
     link: "/blog/0"
